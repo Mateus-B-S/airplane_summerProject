6c982e91a5d3b37387646cf2762a5394bf58c93b
Mateus Buchalla Santos <mbuchalla23@gmail.com>
2026-01-22 18:11:43 -0300

    Update Controle_aviao.ino

--- Controle_aviao/Controle_aviao.ino
+++ Controle_aviao/Controle_aviao.ino
@@ -1,4 +1,6 @@
-//#include <Servo.h>
+#include <SPI.h>
+#include <RF24.h>
+#include <nRF24L01.h>
 
 int YVector_JoystickOne = A0;
 int XVector_JoystickOne = A1;
@@ -7,24 +9,24 @@ int YVector_JoystickTwo = A2;
 
 int XVector_JoystickOne_val, YVector_JoystickOne_val /*, XVector_JoystickTwo_val*/, YVector_JoystickTwo_val;
 
-//no joystick 1
-int profundor = 6;
-int asas = 10;
+//"mensagem" mandada pelo trasmissor e que irá ser recebida pelo receptor (no avião), PRECISA SER O MESMO VALOR!!
+const uint64_t pipeOut = 0xE8E8F0F0E1LL;
 
-//no Joystick 2
-//int leme = 9;
-//int acelerador = 11;
+RF24 radio(9, 10);
 
-//int prof_pos;
-//int asas_pos;
+struct Dados {
+  byte profundor;
+  byte asas;
+  byte motor;
+};
 
-//int leme_pos;
-//int acel_pos;
+Dados comando;
 
-//Servo prof_servo;
-//Servo asas_servo;
-//Servo leme_servo;
-//Servo acel_servo;
+void resetComandos() {  //inializa os motores para suas posições certas.
+  comando.profundor = 120;
+  comando.asas = 0;
+  comando.motor = 0;
+};
 
 void setup() {
   Serial.begin(9600);
@@ -32,47 +34,37 @@ void setup() {
   pinMode(YVector_JoystickOne, INPUT);
   //pinMode(XVector_JoystickTwo, INPUT);
   pinMode(YVector_JoystickTwo, INPUT);
-
-  //prof_servo.attach(profundor);
-  //asas_servo.attach(asas);
-  //leme_servo.attach(leme);
-  //acel_servo.attach(acelerador);
-
+  radio.begin();
+  radio.setAutoAck(false);  //não verifica se recebeu os dados automaticamente, fazendo a comunicação mais rápida
+  radio.setDataRate(RF24_250KBPS);
+  radio.openWritingPipe(pipeOut);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.stopListening();
+  resetComandos();
 };
+
 
 void loop() {
   // put your main code here, to run repeatedly:
 
   XVector_JoystickOne_val = analogRead(XVector_JoystickOne);
   YVector_JoystickOne_val = analogRead(YVector_JoystickOne);
-  XVector_JoystickTwo_val = analogRead(XVector_JoystickTwo);
+  //XVector_JoystickTwo_val = analogRead(XVector_JoystickTwo);
   YVector_JoystickTwo_val = analogRead(YVector_JoystickTwo);
+
+  comando.profundor = map(YVector_JoystickOne, 0, 1023, 0, 120);
+  comando.asas = map(XVector_JoystickOne, 0, 1023, 0, 150);
+  comando.motor = map(YVector_JoystickTwo, 0, 1023, 0, 180);
+
+  radio.write(&comando, sizeof(Dados));
 
   Serial.print("asas: ");
   Serial.print(XVector_JoystickOne_val);
   Serial.print(" | profundor: ");
   Serial.print(YVector_JoystickOne_val);
   Serial.print(" | aceleracao: ");
-  Serial.println(YVector_JoystickTwo_val );
+  Serial.println(YVector_JoystickTwo_val);
 
 
-  /*No joystick 1, o X representa as asas, e o Y, o profundor
-  asas_pos = map(XVector_JoystickOne_val, 0, 1023, 0, 180);
-  prof_pos = map(YVector_JoystickOne_val, 0, 1023, 0, 180); //depois mudar os valores para algo mais coerente
-
-  // No joystick 2, o X representa o leme, e o Y, o acelerador
-  leme_pos = map(XJ2_val, 0, 1023, 0, 180);
-  acel_pos = map(YVector_JoystickTwo_val, 0, 1023, 0, 180);// denovo, mudar os valores para algo mais real
-
-  asas_servo.write(asas_pos);
-  prof_servo.write(prof_pos);
-  leme_servo.write(leme_pos);
-  acel_servo.write(acel_pos);
-
-
- */
-  
-
-
- delay(100);
+  delay(100);
 };
